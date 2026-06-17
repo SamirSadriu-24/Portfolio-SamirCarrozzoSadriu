87567e9e3f2f0be785fef5a79cd52ede2ca85b11
SamirSadriu-24 <220008833+SamirSadriu-24@users.noreply.github.com>
2026-06-17 18:07:42 +0200

    aggiornamento

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/Portfolio-SamirCarrozzoSadriu">
         <MyNavbar />
         <Routes>
           <Route path='/' element={<MyHomePage />} />
